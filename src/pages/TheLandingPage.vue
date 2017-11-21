6ac097d0ac52ee7eb474dbffe4d8bb2cf68b8f58
Marlena Baker <baker.marlena@gmail.com>
2017-11-21 09:08:12 -0700

    end of startup weekend

--- src/pages/TheLandingPage.vue
+++ src/pages/TheLandingPage.vue
@@ -3,11 +3,22 @@
     <div class="hero-content">
       <div class="introduction-text">
         <div>
-          <h3>You want to control the food you eat.</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3>Help us plant the seed for a new urban food revolution.</h3>
+          <p>EatShare provides a service to help neighborhoods set up and maintain gardens in their backyards. With 25 at least houses participating, each neighborhood becomes its own food sharing ecosystem. The excess can be sold back to EatShare.</p>
+          <ul>
+            <li>
+              <a href="https://docs.google.com/presentation/d/1eHmfGmkP_EG5TP4G9CfziwH6b-93xeUBFaxtbj8uqL8/edit?usp=sharing" target="_blank">View our pitch slides</a>
+            </li>
+            <li>
+              <a href="https://www.eventbrite.com/e/startup-weekend-boulder-food-tech-tickets-37108363113" target="_blank">Startup Weekend Boulder Participant</a>
+            </li>
+            <li>
+
+            </li>
+          </ul>
           <!-- <router-link class="store-link" :to="{ name: 'StorePage'}">Shop Tiny Farms</router-link> -->
         </div>
-        <a href="mailto:EMAILADDRESS"><button>Get in touch to learn more!</button></a>
+        <a href="mailto:jasfeldman@gmail.com"><button>Get in touch to learn more!</button></a>
       </div>
       <div>
           <img src="../../static/eat-share-logo.png" alt="EatShare">
@@ -74,5 +85,11 @@ export default {
     /*width: 10rem;*/
     font-size: 2rem;
     padding: .5rem;
+  }
+  ul {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
   }
 </style>
